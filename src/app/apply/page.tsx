a6aa8c392dfecd234463fa00afc34379616db2ba
AshtonFabby <ashtonfabbt@gmail.com>
2025-01-31 13:48:53 +0200

    redirecting to the apply form

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React, { useEffect } from "react";
+const ApplyPage = () => {
+  useEffect(() => {
+    window.location.href = "https://apply.getfunds.co.za";
+  }, []);
+  return null;
+};
+
+export default ApplyPage;
